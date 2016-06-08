07a62e3426d7e5989aa4f48d1d9e7184b84ba3f3
Peter Rupa <megatricycle@gmail.com>
2016-06-08 16:03:13 +0800

    Fix components directory

--- generator_methods.js
+++ generator_methods.js
@@ -1,37 +1,40 @@
 var fs = require('fs-promise');
 
 exports.component = function (name) {
-    var js_path = 'src/components/' + name + '.js',
-        scss_path = 'src/components/' + name + '.scss',
-        test_path = 'src/components/' + name + '.spec.js';
+    var js_path = 'src/components/' + name + '/' + name + '.js',
+        scss_path = 'src/components/' + name + '/' + name + '.scss',
+        test_path = 'src/components/' + name + '/' + name + '.spec.js';
 
-    // write js
-    fs.readFile('templates/component/component.js', 'utf8')
-        .then(function(body) {
-            // transform ${name}
-            return Promise.resolve(body.replace(/\${name}/g, name));
-        })
-        .then(function(body) {
-            // write file
-            fs.writeFile(
-                js_path,
-                body);
-        });
+    fs.mkdir('src/components/' + name)
+        .then(function() {
+            // write js
+            fs.readFile('templates/component/component.js', 'utf8')
+                .then(function(body) {
+                    // transform ${name}
+                    return Promise.resolve(body.replace(/\${name}/g, name));
+                })
+                .then(function(body) {
+                    // write file
+                    fs.writeFile(
+                        js_path,
+                        body);
+                });
 
-    // write scss
-    fs.writeFile(scss_path, '');
+            // write scss
+            fs.writeFile(scss_path, '');
 
-    // write test
-    fs.readFile('templates/component/component.spec.js', 'utf8')
-        .then(function(body) {
-            // transform ${name}
-            return Promise.resolve(body.replace(/\${name}/g, name));
-        })
-        .then(function(body) {
-            // write file
-            fs.writeFile(
-                test_path,
-                body);
+            // write test
+            fs.readFile('templates/component/component.spec.js', 'utf8')
+                .then(function(body) {
+                    // transform ${name}
+                    return Promise.resolve(body.replace(/\${name}/g, name));
+                })
+                .then(function(body) {
+                    // write file
+                    fs.writeFile(
+                        test_path,
+                        body);
+                });
         });
 }
 
